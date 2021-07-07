f420fbe300c82f9c5916e90291378344c42f360e
Dan Pettersson <dan82pettersson@gmail.com>
2021-07-07 16:41:53 +0200

    MainWeb; Rename app -> app-entry

---
+++ src/FooBarMainWeb/app/app-entry.ts
@@ -0,0 +1,13 @@
+import { route } from "@aurelia/router";
+import Aurelia, { DI } from "aurelia";
+import { MvcRoute } from 'SHARED/routing/mvc-route';
+
+@route({
+    routes: [
+        { path: '', /*redirectTo: '',*/ component: MvcRoute }, //RedirectTo not working, handled in mvc-route component for now...
+        { path: ':mvcController/:mvcAction/:id?', component: MvcRoute }
+    ]
+})
+export class AppEntry {
+    message: string = 'Hello World!';
+}
